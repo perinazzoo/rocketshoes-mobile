2b0de2c791cc0754bc214c682956ed5e923481ec
perinazzoo <gabrielperinazo@gmail.com>
2020-02-29 06:07:17 -0300

    :construction: add cart list with redux data

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,7 +1,7 @@
 export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD':
-      return [...state, action.product];
+      return [...state, { ...action.product, amount: 1 }];
     default:
       return state;
   }
