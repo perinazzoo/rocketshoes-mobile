daef0549ab7bdb0287ae7a59768fda56a3857623
perinazzoo <gabrielperinazo@gmail.com>
2020-03-09 17:44:18 -0300

    :recycle: change all class component and redux props to hooks

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
 
-import * as CartActions from '../../store/modules/cart/actions';
+import { addToCartRequest } from '../../store/modules/cart/actions';
+import { isLoading } from '../../store/modules/loading/actions';
 import api from '../../services/api';
 import { formatPrice } from '../../util/format';
 
@@ -23,96 +22,79 @@ import {
   ButtonText,
 } from './styles';
 
-class Home extends Component {
-  // eslint-disable-next-line react/state-in-constructor
-  state = {
-    products: [],
+export default function Home() {
+  const [products, setProducts] = useState([]);
+  const amount = useSelector(({ cart }) =>
+    cart.reduce((a, p) => {
+      a[p.id] = p.amount;
+
+      return a;
+    }, {})
+  );
+
+  const loadingStatus = useSelector(({ loading }) => loading);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(isLoading(true));
+    async function loadProducts() {
+      const response = await api.get('/products');
+
+      const data = response.data.map(p => ({
+        ...p,
+        priceFormatted: formatPrice(p.price),
+      }));
+      setProducts(data);
+      dispatch(isLoading(false));
+    }
+    loadProducts();
+  }, []);
+
+  const handleAddProduct = (id, index) => {
+    dispatch(addToCartRequest(id, index));
   };
 
-  async componentDidMount() {
-    const response = await api.get('/products');
-
-    const data = response.data.map(p => ({
-      ...p,
-      priceFormatted: formatPrice(p.price),
-    }));
-
-    this.setState({ products: data });
-  }
-
-  handleAddProduct = (id, index) => {
-    const { addToCartRequest } = this.props;
-
-    addToCartRequest(id, index);
-  };
-
-  render() {
-    const { products } = this.state;
-    const { amount, isloading } = this.props;
-
-    return (
-      <Container>
-        <ListContainer>
-          <FlatList
-            showsHorizontalScrollIndicator={false}
-            horizontal
-            data={products}
-            keyExtractor={product => product.title}
-            renderItem={({ item, index }) => (
-              <ProductContainer>
-                <ProductImage
-                  source={{
-                    uri: item.image,
-                  }}
-                />
-                <ProductTitle>{item.title}</ProductTitle>
-                <ProductPrice>{item.priceFormatted}</ProductPrice>
-                <SubmitButton
-                  isloading={isloading.state}
-                  onPress={() => this.handleAddProduct(item.id, index)}
-                >
-                  {isloading.index === index && isloading.state === true ? (
+  return (
+    <Container>
+      <ListContainer>
+        <FlatList
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          data={products}
+          keyExtractor={product => product.title}
+          renderItem={({ item, index }) => (
+            <ProductContainer>
+              <ProductImage
+                source={{
+                  uri: item.image,
+                }}
+              />
+              <ProductTitle>{item.title}</ProductTitle>
+              <ProductPrice>{item.priceFormatted}</ProductPrice>
+              <SubmitButton
+                isloading={loadingStatus.state}
+                onPress={() => handleAddProduct(item.id, index)}
+              >
+                {loadingStatus.index === index &&
+                  loadingStatus.state === true ? (
                     <SubmitButtonLoading>
                       <ActivityIndicator color="#fff" size={30} />
                     </SubmitButtonLoading>
                   ) : (
-                      <>
-                        <CartButtonContainer>
-                          <Icon name="add-shopping-cart" size={16} color="#fff" />
-                          <CartCount>{amount[item.id] || 0}</CartCount>
-                        </CartButtonContainer>
-                        <ButtonText>Adicionar</ButtonText>
-                      </>
-                    )}
-                </SubmitButton>
-              </ProductContainer>
-            )}
-          />
-        </ListContainer>
-      </Container>
-    );
-  }
+                    <>
+                      <CartButtonContainer>
+                        <Icon name="add-shopping-cart" size={16} color="#fff" />
+                        <CartCount>{amount[item.id] || 0}</CartCount>
+                      </CartButtonContainer>
+                      <ButtonText>Adicionar</ButtonText>
+                    </>
+                  )}
+              </SubmitButton>
+            </ProductContainer>
+          )}
+        />
+      </ListContainer>
+    </Container>
+  );
 }
-
-Home.propTypes = {
-  addToCartRequest: PropTypes.func.isRequired,
-  amount: PropTypes.objectOf(Number).isRequired,
-  isloading: PropTypes.shape({
-    index: PropTypes.number,
-    state: PropTypes.bool,
-  }).isRequired,
-};
-
-const mapStateToProps = ({ cart, loading }) => ({
-  amount: cart.reduce((amount, p) => {
-    amount[p.id] = p.amount;
-
-    return amount;
-  }, {}),
-  isloading: loading,
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(CartActions, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
